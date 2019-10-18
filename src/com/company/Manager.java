95a2aeba0d435e54140ed600e23e2f55f09db922
Derananer <10ilay1998@gmail.com>
2019-10-19 02:20:39 +0400

    реализация полногго перебора ребер для звездного покрытия

--- src/com/company/Manager.java
+++ src/com/company/Manager.java
@@ -4,42 +4,54 @@ import java.math.BigInteger;
 import java.util.Collections;
 import java.util.List;
 
-public class Manager {
+public class Manager
+{
 
-    public static void bruteForce(List<Vertex> vertexList, int starsCount) {
+    public static void bruteForce(List<Vertex> vertexList, int starsCount)
+    {
         int[] arr = null;
-        while ((arr = generateCombinations(arr, starsCount, vertexList.size())) != null) {
+        while ((arr = generateCombinations(arr, starsCount, vertexList.size())) != null)
+        {
 
         }
 
 
     }
 
-    public static int L(List<Vertex> vertexList) {
+
+    //todo rename naxui
+    public static int L(List<Vertex> vertexList)
+    {
         int count = 0;
         for (Vertex vertex :
-                vertexList) {
+                vertexList)
+        {
             for (Edge edge :
-                    vertex.getEdges()) {
+                    vertex.getEdges())
+            {
                 count += vertex.getWeight() * edge.getWeight();
             }
         }
         return count;
     }
 
-    public static int getPlan(List<Vertex> vertexList) {
+    public static int getPlan(List<Vertex> vertexList)
+    {
         return L(Collections.singletonList(vertexList.get(0)));
     }
 
-    private static int[] generateCombinations(int[] arr, int starCount, int vertexCount) {
-        if (arr == null) {
+    private static int[] generateCombinations(int[] arr, int starCount, int vertexCount)
+    {
+        if (arr == null)
+        {
             arr = new int[starCount];
             for (int i = 0; i < starCount; i++)
                 arr[i] = i + 1;
             return arr;
         }
         for (int i = starCount - 1; i >= 0; i--)
-            if (arr[i] < vertexCount - starCount + i + 1) {
+            if (arr[i] < vertexCount - starCount + i + 1)
+            {
                 arr[i]++;
                 for (int j = i; j < starCount - 1; j++)
                     arr[j + 1] = arr[j] + 1;
