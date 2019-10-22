ecaf1aa947a670f2c79b9e1617b7ec9181b0fe03
Derananer <10ilay1998@gmail.com>
2019-10-22 23:57:33 +0400

    new edgematrix logic start

--- src/main/java/com/company/EdgeMatrix.java
+++ src/main/java/com/company/EdgeMatrix.java
@@ -2,4 +2,15 @@ package com.company;
 
 public class EdgeMatrix
 {
+    private int[][] incidenceMatrix;
+
+    public EdgeMatrix(int n, int m)
+    {
+
+    }
+
+    public void setCell(int i, int j, int value){
+
+    }
+
 }
