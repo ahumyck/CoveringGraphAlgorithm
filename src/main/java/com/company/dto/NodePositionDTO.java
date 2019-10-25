5f229fa15e7d730e8b96d3225dcd226faab9b781
ahumyck <ahumyckbf4@gmail.com>
2019-10-26 01:14:12 +0400

    node position of vertex added

---
+++ src/main/java/com/company/dto/NodePositionDTO.java
@@ -0,0 +1,13 @@
+package com.company.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class NodePositionDTO {
+    private int x;
+    private int y;
+}
