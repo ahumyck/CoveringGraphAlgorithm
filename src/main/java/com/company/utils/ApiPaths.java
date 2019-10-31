62080eb0eab8cd5e3a9dc5a1bc6280b210ff7ebc
ahumyck <ahumyckbf4@gmail.com>
2019-11-01 03:11:29 +0400

    initial graph path added

--- src/main/java/com/company/utils/ApiPaths.java
+++ src/main/java/com/company/utils/ApiPaths.java
@@ -3,12 +3,14 @@ package com.company.utils;
 public class ApiPaths
 {
     public static final String
-            BRUTE_FORSE_PATH = "/brute-force",
+            BRUTE_FORCE_PATH = "/brute-force",
             INITIAL_GRAPH_PATH = "/initial-graph",
             EXECUTE_PATH = "/execute",
             ADDITIVE_PATH = "/additive",
             NUMBER_PATH = "/{number}",
             END_PATH = "/end",
-            SOLUTION_PATH = "/solution";
+            SOLUTION_PATH = "/solution",
+            POST_FILE = "/post-file";
+
 
 }
