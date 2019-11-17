2a258343f226a0e6cb68506a02a7627712e56a19
Derananer <10ilay1998@gmail.com>
2019-11-17 22:43:04 +0400

    super puper zjadina by lisa

--- src/main/java/com/company/DemoApplication.java
+++ src/main/java/com/company/DemoApplication.java
@@ -7,6 +7,7 @@ import com.company.generators.CombinationBinaryGeneratorLong;
 import com.company.parsers.GraphParser;
 import com.company.services.coefficientsBuilder.LinearCoefficientsBuilder;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -14,15 +15,31 @@ import java.util.Map;
 
 
 @SpringBootApplication
-public class DemoApplication {
-    public static void main(String[] args) throws FileNotFoundException {
+public class DemoApplication
+{
+    public static void main(String[] args) throws FileNotFoundException
+    {
 //        SpringApplication.run(DemoApplication.class, args);
-        Graph graph = GraphParser
-                .parseFile("C:\\Users\\Илья\\Desktop\\nauchka_sb\\CoveringGraphAlgorithm\\src\\main\\resources\\matrixData\\6x6graph.txt");
-        int n = graph.size(); // 8*7
-        ArrayList<Coefficient> coefficients = new LinearCoefficientsBuilder().build(graph).orderByWeight().getCoefficients();
+        Graph graphSized8 = GraphParser
+                .parseFile("C:\\Users\\ILYA\\nauchka\\CoveringGraphAlgorithm\\src\\main\\resources\\matrixData\\16x16graph.txt");
+        int n = graphSized8.size(); // 8*7
+        ArrayList<Coefficient> coefficients = new LinearCoefficientsBuilder().build(graphSized8).orderByWeight().getCoefficients();
+        long l = System.currentTimeMillis();
+//        BruteForceAlgorithmTest.solve(graphSized8, coefficients, 2, n);
+        System.out.println(System.currentTimeMillis() - l);
         Map<Integer, ArrayList<Integer>> solve = GreedyAlgorithmTest.solve(coefficients, n);
+        Map<Coefficient, ArrayList<Coefficient>> mySolve = GreedyTest.solve(coefficients, 4, n);
+        System.out.println("my");
+        mySolve.forEach((key, value) ->
+        {
+            System.out.print(key.getStar() + "=[");
+            value.forEach(x -> System.out.print(x.getSatellite() + ","));
+            System.out.print("]");
+            System.out.println();
+        });
+        System.out.println("result: " + GreedyTest.calculate(mySolve));
+        System.out.println("my");
         System.out.println(solve);
-        System.out.println(BruteForceAlgorithmTest.calculate(solve,graph));
+        System.out.println(BruteForceAlgorithmTest.calculate(solve, graphSized8));
     }
 }
---
+++ src/main/java/com/company/GreedyTest.java
@@ -0,0 +1,64 @@
+package com.company;
+
+import com.company.entities.Coefficient;
+import com.company.entities.EdgeMatrix;
+import com.company.entities.Graph;
+
+import java.util.*;
+
+public class GreedyTest
+{
+
+    public static Map<Coefficient, ArrayList<Coefficient>> solve(List<Coefficient> coefficients, int starCount, int vertexCount)
+    {
+        List<Coefficient> listStars = new ArrayList<>(starCount);
+        List<Integer> listStarsIndex = new ArrayList<>(starCount);
+        List<Integer> listSatellites = new ArrayList<>();
+        Map<Coefficient, ArrayList<Coefficient>> mapStars = new HashMap<>(starCount);
+
+        mapStars.put(coefficients.get(0), new ArrayList<>());
+        mapStars.get(coefficients.get(0)).add(coefficients.get(0));
+        listStars.add(coefficients.get(0));
+        listStarsIndex.add(coefficients.get(0).getStar());
+        listSatellites.add(coefficients.get(0).getSatellite());
+        for (int i = 1; i < coefficients.size(); i++)
+        {
+            if (vertexCount <= listSatellites.size() + listStarsIndex.size()) break;
+            boolean flag = true;
+            for (int j = 0; j < listStars.size(); j++)
+            {
+                if (listStars.get(j).getStar() == coefficients.get(i).getStar() && !listSatellites.contains(coefficients.get(i).getSatellite()) && !listStarsIndex.contains(coefficients.get(i).getSatellite()))
+                {
+                    mapStars.get(listStars.get(j)).add(coefficients.get(i));
+                    listSatellites.add(coefficients.get(i).getSatellite());
+                    flag = false;
+                    break;
+                }
+//                else
+//                {
+//                    mapStars.put(coefficients.get(i), new ArrayList<>());
+//                }
+            }
+            if (flag && starCount > listStars.size() && !listSatellites.contains(coefficients.get(i).getStar()) && !listStarsIndex.contains(coefficients.get(i).getStar()))
+            {
+
+                    mapStars.put(coefficients.get(i), new ArrayList<>());
+                    listStars.add(coefficients.get(i));
+                    listStarsIndex.add(coefficients.get(i).getStar());
+                    if (!listSatellites.contains(coefficients.get(i).getSatellite()) && !listStarsIndex.contains(coefficients.get(i).getSatellite()))
+                    {
+                    mapStars.get(coefficients.get(i)).add(coefficients.get(i));
+                    listSatellites.add(coefficients.get(i).getSatellite());
+                }
+            }
+        }
+        return mapStars;
+    }
+
+    public static long calculate(Map<Coefficient, ArrayList<Coefficient>> map)
+    {
+        final long[] sum = new long[1];
+        map.forEach((key, value) -> value.forEach(x -> sum[0] += x.getWeight()));
+        return sum[0];
+    }
+}
