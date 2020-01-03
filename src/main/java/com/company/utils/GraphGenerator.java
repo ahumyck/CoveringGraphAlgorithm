a9a0f25ff338e8a379094bf5d77d649ebb62b9b1
Derananer <10ilay1998@gmail.com>
2020-01-03 16:56:09 +0400

    generate

---
+++ src/main/java/com/company/utils/GraphGenerator.java
@@ -0,0 +1,33 @@
+package com.company.utils;
+
+        import com.company.entities.EdgeMatrix;
+        import com.company.entities.Graph;
+        import com.company.entities.Vertex;
+
+        import java.util.ArrayList;
+        import java.util.List;
+        import java.util.Random;
+
+public class GraphGenerator
+{
+    private static final Random VERTEX_WEIGHT_RANDOM = new Random();
+    private static final Random EDGE_WEIGHT_RANDOM = new Random();
+
+
+    public static Graph generate(int vertexCount, int maxVertexWeight, int maxEdgeWeight)
+    {
+        List<Vertex> vertices = new ArrayList<>();
+        for (int i = 0; i < vertexCount; i++)
+        {
+            vertices.add(new Vertex(i, VERTEX_WEIGHT_RANDOM.nextInt(maxVertexWeight) + 1));
+        }
+        EdgeMatrix matrix = new EdgeMatrix(vertexCount);
+        for (int i = 0; i < vertexCount ; i++) {
+            for (int j = 0; j < vertexCount; j++) {
+                matrix.setCell(i,j, EDGE_WEIGHT_RANDOM.nextInt(maxEdgeWeight) + 1);
+            }
+        }
+        return new Graph(vertices, matrix);
+    }
+
+}
